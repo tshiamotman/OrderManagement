9d184645ac7a19ea75d941bb0f92ee6e7c0dfc88
tshiamotman <tshiamo.lephale@gmail.com>
2023-03-22 14:48:41 +0200

    first commit

---
+++ src/main/java/co/za/ordermanagement/OrderManagementApplication.java
@@ -0,0 +1,13 @@
+package co.za.ordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementApplication.class, args);
+    }
+
+}
---
+++ src/main/java/co/za/ordermanagement/config/MigrationConfiguration.java
@@ -0,0 +1,5 @@
+package co.za.ordermanagement.config;
+
+
+public class MigrationConfiguration {
+}
